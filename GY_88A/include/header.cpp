23cb5cf2aeae48b3400d9fa5f215f3f48f15ae26
Stefan Clementz <Stefan.Clementz@gmail.com>
2020-01-01 21:31:18 +0100

    Delete embarrasing codes

--- GY_88A/include/header.cpp
+++
@@ -1,8 +0,0 @@
-#ifndef HEADER_CPP
-#define HEADER_CPP
-
-
-
-#include "GY-88A.hpp"
-
-#endif
--- I2C/Master/include/header.hpp
+++
@@ -1,8 +0,0 @@
-#include <stdint.h>
-#include <stdio.h>
-#include <libopencm3/stm32/rcc.h>
-#include <libopencm3/stm32/gpio.h>
-#include <libopencm3/stm32/i2c.h>
-
-#include "millis.hpp"
-#include "I2C.hpp"
